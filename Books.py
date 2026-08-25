8faf907583eac3f90bb8d3c4f546dae0f53f62ce
omid tohidi <tohidi@socib.co>
2026-08-25 11:21:37 +0330

    15. FastAPI Project Delete Request

--- Books.py
+++ Books.py
@@ -92,3 +92,11 @@ async def create_book(new_book=Body()):
 async def update_book(updated_book=Body()):
     for i in range(len(books)):
         books[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(books)):
+        if books[i].get("title").casefold() == book_title.casefold():
+            books.pop(i)
+            break
