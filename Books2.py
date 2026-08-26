8e55f5dee2c5d801923fd5957ed36539dac5b7fe
omid tohidi <otohidi@live.com>
2026-08-27 01:34:59 +0330

    8. FastAPI Project Pydantic Configurations

--- Books2.py
+++ Books2.py
@@ -15,11 +15,22 @@ class Books:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description='ID is not needed on create', default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "title": "A new book",
+                "author": "codingwithomid",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
 
 
 BOOKS = [
