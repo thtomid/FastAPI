74b481063765fd6784fc44ecb11d6a954710df6f
omid tohidi <otohidi@live.com>
2026-08-26 17:19:50 +0330

    4. FastAPI Project Post Request before Validation

--- Books2.py
+++ Books2.py
@@ -67,3 +67,7 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request=Body()):
+    BOOKS.append(book_request)
