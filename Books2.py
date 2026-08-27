3dd9613c635837586664049fcd99d447fb642849
omid tohidi <otohidi@live.com>
2026-08-27 16:50:37 +0330

    10. FastAPI Project Fetch Book

--- Books2.py
+++ Books2.py
@@ -90,6 +90,12 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Books(**book_request.dict())
