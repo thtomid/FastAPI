bed454bed8edbe3fdd0646af881ce87bb234dd2c
omid tohidi <otohidi@live.com>
2026-08-30 14:43:32 +0330

    17. FastAPI Project Data Validation Query Parameters

--- Books2.py
+++ Books2.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI, Body, Path
+from fastapi import FastAPI, Body, Path, Query
 from pydantic import BaseModel, Field
 app = FastAPI()
 
@@ -100,7 +100,7 @@ async def read_book(book_id: int = Path(gt=0)):
             return book
 
 @app.get("/books/")
-async def read_book_by_rating(book_rating: int):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -108,7 +108,7 @@ async def read_book_by_rating(book_rating: int):
     return books_to_return
 
 @app.get("/books/publish/")
-async def read_books_by_publish_date(published_date: int):
+async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_to_return = []
     for book in BOOKS:
         if book.published_date == published_date:
