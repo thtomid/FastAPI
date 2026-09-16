ea22f875391faabda648f75d9981b26df863699d
Omid Tohidi <otohidi@live.com>
2026-09-16 21:45:05 +0330

    9. API Request Methods

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[SessionLocal, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(models.Todos).all()
