339a8b7b22e4e5aca82591bf6ffe9195df2dc117
omid tohidi <otohidi@live.com>
2026-09-17 19:25:05 +0330

    4. FastAPI Project Put Request (Todo Project)

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -34,7 +34,7 @@ async def read_all(db: db_dependency):
     return db.query(models.Todos).all()
 
 
-@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_one(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is not None:
@@ -42,9 +42,23 @@ async def read_one(db: db_dependency, todo_id: int = Path(gt=0)):
     raise HTTPException(status_code=404, detail="Todo not Found.")
 
 
-@app.post("/todos", status_code=status.HTTP_201_CREATED)
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.dict())
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not Found.')
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.completed = todo_request.completed
 
     db.add(todo_model)
     db.commit()
