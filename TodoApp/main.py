6bd3fbb70c7cfd71c280322b7973bcc510c210cf
omid tohidi <otohidi@live.com>
2026-09-18 00:55:17 +0330

    5. FastAPI Project Delete Request (Todo Project)

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -62,3 +62,13 @@ async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int
 
     db.add(todo_model)
     db.commit()
+
+
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not Found.')
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+
+    db.commit()
