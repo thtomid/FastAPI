ff00f985ad11c27a659c802ce8ae2465f793fd58
omid tohidi <otohidi@live.com>
2026-09-22 19:08:25 +0330

    2. FastAPI Project Routers Scale Authentication File

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -7,11 +7,13 @@ from starlette import status
 import models
 from models import Todos
 from database import engine, SessionLocal
+from routers import auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+app.include_router(auth.router)
 
 def get_db():
     db = SessionLocal()
