ef788cee89feacd7d4fbcc21e6e0997505687506
omid tohidi <otohidi@live.com>
2026-09-26 15:27:02 +0330

    6. FastAPI Project Users Table Creation

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,17 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -7,4 +19,5 @@ class Todos(Base):
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    completed = Column(Boolean, default=False)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
